9e8b4871841e27238268bba44fddbe1fe504f7dc
Dom Webber <dom.webber@hotmail.com>
2024-12-01 12:32:43 +0000

    Add day 1

--- 01/main.ts
+++ 01/main.ts
@@ -1,8 +1,45 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import inputA from "./input-a.json" with { type: "json" };
+import inputB from "./input-b.json" with { type: "json" };
+
+export function sumDistances(...lists: [number[], number[]]): number {
+  const pairs: number[][] = [];
+
+  for (const list of lists) {
+    const sorted = list.sort();
+
+    for (const [i, item] of sorted.entries()) {
+      pairs[i] = [item, ...(pairs[i] ?? [])];
+    }
+  }
+
+  let distance = 0;
+  for (const pair of pairs) {
+    distance += pair.reduce((previous, current) => Math.abs(previous - current));
+  }
+
+  return distance;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+export function similarityScore(listA: number[], listB: number[]): number {
+  const setA = new Set(listA);
+  const frequencies = new Map<number, number>();
+
+  for (const item of listB) {
+    if (!setA.has(item)) continue;
+    const existingFrequency = frequencies.get(item) ?? 0;
+    frequencies.set(item, existingFrequency + 1);
+  }
+
+  let similarityScore = 0;
+  for (const item of listA) {
+    const itemFrequency = frequencies.get(item) ?? 0;
+    similarityScore += item * itemFrequency;
+  }
+
+  return similarityScore;
+}
+
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Sum of Distances =", sumDistances(inputA, inputB));
+  console.log("Similarity Score =", similarityScore(inputA, inputB));
 }
--- 01/main_test.ts
+++ 01/main_test.ts
@@ -1,6 +1,23 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { sumDistances, similarityScore } from "./main.ts";
+import inputA from "./input-a.json" with { type: "json" };
+import inputB from "./input-b.json" with { type: "json" };
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const demoA = [3, 4, 2, 1, 3, 3];
+const demoB = [4, 3, 5, 3, 9, 3];
+
+Deno.test(function sumDistancesTest() {
+  assertEquals(sumDistances(demoA, demoB), 11);
+});
+
+Deno.test(function sumDistancesTest() {
+  assertEquals(sumDistances(inputA, inputB), 765748);
+});
+
+Deno.test(function sumDistancesTest() {
+  assertEquals(similarityScore(demoA, demoB), 31);
+});
+
+Deno.test(function sumDistancesTest() {
+  assertEquals(similarityScore(inputA, inputB), 27732508);
 });
