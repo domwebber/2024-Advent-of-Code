7600ba6ac59bbabc156e5993041bcf5299b35ddc
Dom Webber <dom.webber@hotmail.com>
2024-12-02 08:56:34 +0000

    Remove testing with actual answers

--- 01/main_test.ts
+++ 01/main_test.ts
@@ -1,7 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { sumDistances, similarityScore } from "./main.ts";
-import inputA from "./input-a.json" with { type: "json" };
-import inputB from "./input-b.json" with { type: "json" };
+import { similarityScore, sumDistances } from "./main.ts";
 
 const demoA = [3, 4, 2, 1, 3, 3];
 const demoB = [4, 3, 5, 3, 9, 3];
@@ -11,13 +9,5 @@ Deno.test(function sumDistancesTest() {
 });
 
 Deno.test(function sumDistancesTest() {
-  assertEquals(sumDistances(inputA, inputB), 765748);
-});
-
-Deno.test(function sumDistancesTest() {
   assertEquals(similarityScore(demoA, demoB), 31);
-});
-
-Deno.test(function sumDistancesTest() {
-  assertEquals(similarityScore(inputA, inputB), 27732508);
 });
